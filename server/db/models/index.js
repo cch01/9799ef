9690b0b0dd4ecaf4bb2f1731719c3a03b96c22f7
Terry Chan <cch01@protonmail.com>
2022-01-10 18:00:36 -0500

    feat: updated DB models for group chat support

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,22 @@
 const Conversation = require('./conversation');
 const User = require('./user');
 const Message = require('./message');
+const UserConversation = require('./userConversation');
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: 'user1' });
-Conversation.belongsTo(User, { as: 'user2' });
+UserConversation.belongsTo(User);
+UserConversation.belongsTo(Conversation);
+
+Conversation.belongsToMany(User, {
+  through: UserConversation,
+  foreignKey: 'conversationId',
+});
+User.belongsToMany(Conversation, {
+  through: UserConversation,
+  foreignKey: 'userId',
+});
+
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
---
+++ server/db/models/userConversation.js
@@ -0,0 +1,5 @@
+const db = require('../db');
+
+const UserConversation = db.define('userConversations', {});
+
+module.exports = UserConversation;
